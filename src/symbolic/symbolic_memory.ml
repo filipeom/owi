c80a86ef242ea88a40d6df003af89f5ce9fd083f
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-02-27 14:40:13 +0000

    Use smtml's extract and concat operators

--- src/symbolic/symbolic_memory.ml
+++ src/symbolic/symbolic_memory.ml
@@ -4,7 +4,6 @@
 
 module Value = Symbolic_value
 module Expr = Smtml.Expr
-module Ty = Smtml.Ty
 open Expr
 
 let page_size = Symbolic_value.const_i32 65_536l
@@ -75,42 +74,13 @@ let rec load_byte { parent; data; _ } a =
   end
   | Some v -> v
 
-(* TODO: don't rebuild so many values it generates unecessary hc lookups *)
-let merge_extracts (e1, h, m1) (e2, m2, l) =
-  let ty = Expr.ty e1 in
-  if m1 = m2 && Expr.equal e1 e2 then
-    if h - l = Ty.size ty then e1 else make (Extract (e1, h, l))
-  else make (Concat (make (Extract (e1, h, m1)), make (Extract (e2, m2, l))))
-
-let concat ~msb ~lsb offset =
-  assert (offset > 0 && offset <= 8);
-  match (view msb, view lsb) with
-  | Val (Num (I8 i1)), Val (Num (I8 i2)) ->
-    Value.const_i32 Int32.(logor (shl (of_int i1) 8l) (of_int i2))
-  | Val (Num (I8 i1)), Val (Num (I32 i2)) ->
-    let offset = offset * 8 in
-    if offset < 32 then
-      Value.const_i32 Int32.(logor (shl (of_int i1) (of_int offset)) i2)
-    else
-      let i1' = Int64.of_int i1 in
-      let i2' = Int64.of_int32 i2 in
-      Value.const_i64 Int64.(logor (shl i1' (of_int offset)) i2')
-  | Val (Num (I8 i1)), Val (Num (I64 i2)) ->
-    let offset = Int64.of_int (offset * 8) in
-    Value.const_i64 Int64.(logor (shl (of_int i1) offset) i2)
-  | Extract (e1, h, m1), Extract (e2, m2, l) ->
-    merge_extracts (e1, h, m1) (e2, m2, l)
-  | Extract (e1, h, m1), Concat ({ node = Extract (e2, m2, l); _ }, e3) ->
-    make (Concat (merge_extracts (e1, h, m1) (e2, m2, l), e3))
-  | _ -> make (Concat (msb, lsb))
-
 let loadn m a n =
   let rec loop addr size i acc =
     if i = size then acc
     else
       let addr' = Int32.(add addr (of_int i)) in
       let byte = load_byte m addr' in
-      loop addr size (i + 1) (concat i ~msb:byte ~lsb:acc)
+      loop addr size (i + 1) (Smtml.Expr.concat3 i ~msb:byte ~lsb:acc)
   in
   let v0 = load_byte m a in
   loop a n 1 v0
@@ -143,26 +113,11 @@ let load_32 m a = loadn m (i32 a) 4
 
 let load_64 m a = loadn m (i32 a) 8
 
-let extract v pos =
-  match view v with
-  | Val (Num (I32 i)) ->
-    let i' = Int32.(to_int @@ logand 0xffl @@ shr_s i @@ of_int (pos * 8)) in
-    value (Num (I8 i'))
-  | Val (Num (I64 i)) ->
-    let i' = Int64.(to_int @@ logand 0xffL @@ shr_s i @@ of_int (pos * 8)) in
-    value (Num (I8 i'))
-  | Cvtop
-      ( _
-      , (Zero_extend 24 | Sign_extend 24)
-      , ({ node = Symbol { ty = Ty_bitv 8; _ }; _ } as sym) ) ->
-    sym
-  | _ -> make (Extract (v, pos + 1, pos))
-
 let storen m ~addr v n =
   let a0 = i32 addr in
   for i = 0 to n - 1 do
     let addr' = Int32.add a0 (Int32.of_int i) in
-    let v' = extract v i in
+    let v' = Smtml.Expr.extract2 v i in
     Hashtbl.replace m.data addr' v'
   done
 
